c9afef59124e43de57d08f68d07a0b86105d37af
michael belete <it.michael.belete@gmail.com>
2021-02-18 02:56:17 +0300

    add code scanner

--- app/src/main/java/com/runtimeTerror/idvs/MainActivity.kt
+++ app/src/main/java/com/runtimeTerror/idvs/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.runtimeTerror.idvs
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val scanBtn = findViewById<Button>(R.id.scanId)
+
+        scanBtn.setOnClickListener {
+            val intent = Intent(this, scannerActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
